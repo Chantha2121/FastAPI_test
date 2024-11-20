577d9c1223da7f2a74d83828ce13c8b98898a75a
sunchengchhay <sonchengchhay@gmail.com>
2024-11-20 15:22:59 +0700

    update code

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-app.get('/')
+@app.get('/')
 def hello():
-    return {"data": [1,2,34,545,90]}
+    return {"data": [1, 2, 34, 545, 90]}
 
-app.get('/hello')
+@app.get('/hello')
 def hi():
     return {"message": "Hello, World!"}
